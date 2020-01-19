241b258358d248a943396a23c8e3324accf3b9bd
programer20 <denis2607@hotmail.com>
2020-01-19 21:41:19 +0100

    Kreiranje URL-a za svaku stranicu pretrage i skupljanje rezultata sa svake od njih

--- src/clojure_project/core.clj
+++ src/clojure_project/core.clj
@@ -1,8 +1,6 @@
 (ns clojure-project.core
   (:require [net.cgrand.enlive-html :as enlive]))
 
-(def URL "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/izdavanje/cena/0_300/lista/po-stranici/10/stranica/23/")
-
 (defn html-data
   [url]
   (-> url
@@ -47,24 +45,34 @@
   [row]
   (str "https://www.nekretnine.rs" (get-in (first (enlive/select row [:h2 :a])) [:attrs :href])))
 
-(defn -main
-  "I don't do a whole lot...yet."
-  [& args]
-  (println (get-page-count (get-result-count (html-data URL)))))
+(defn construct-city-part
+  [req]
+  (if-not (clojure.string/blank? (:cityPart req))
+    (str "/deo-grada/" (clojure.string/replace (:cityPart req) " " "-"))))
 
-(defn -main
-  [& args]
-  (doseq [i (into [] (get-rows (html-data URL)))] (println (get-href i))))
+(defn construct-city
+  [req]
+  (if-not (clojure.string/blank? (:city req))
+    (str "/grad/" (clojure.string/replace (:city req) " " "-"))))
 
-(defn -main
-  [& args]
-  (def map-list (for [i (get-rows (html-data URL))] {:name (get-name i) :price (get-price i) :surface (get-surface i)
-                                                     :location (get-location i) :href (get-href i)}))
-  (println map-list))
+(defn construct-price
+  [req]
+  (if-not (and (= 0 (:minPrice req)) (= 0 (:maxPrice req)))
+    (str "/cena/" (:minPrice req) "_" (:maxPrice req))))
 
-(defn -main
-  [& args]
-  (def rezultat (mapv (fn [row] {:name (get-name row) :price (get-price row) :surface (get-surface row)
-                          :location (get-location row) :href (get-href row)}) (get-rows (html-data URL))))
-  (println rezultat)
+(defn construct-base-url
+  [req]
+  (str "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/izdavanje"
+       (construct-city-part req) (construct-city req) (construct-price req) "/lista/po-stranici/10/")
   )
+
+(defn construct-url-list
+  [url page-count]
+  (map #(str url "stranica/" %) (range 1 (inc page-count)))
+  )
+
+(defn get-results
+  [url-list]
+  (def rows (reduce concat '() (map #(get-rows (html-data %)) url-list)))
+  (map (fn [row] {:name (get-name row) :price (get-price row) :surface (get-surface row)
+                               :location (get-location row) :href (get-href row)}) rows))
--- src/clojure_project/web.clj
+++ src/clojure_project/web.clj
@@ -1,5 +1,6 @@
 (ns clojure-project.web
-  (:require [compojure.route :as route]
+  (:require [clojure-project.core :as core]
+            [compojure.route :as route]
             [ring.middleware.cors :refer [wrap-cors]]
             [ring.util.http-response :as http])
   (:use compojure.core
@@ -24,34 +25,18 @@
    )
   )
 
-(defn construct-city-part
+(defn search
   [req]
-  (if-not (clojure.string/blank? (:cityPart req))
-    (str "/deo-grada/" (clojure.string/replace (:cityPart req) " " "-"))))
-
-(defn construct-city
-  [req]
-  (if-not (clojure.string/blank? (:city req))
-    (str "/grad/" (clojure.string/replace (:city req) " " "-"))))
-
-(defn construct-price
-  [req]
-  (if-not (and (= 0 (:minPrice req)) (= 0 (:maxPrice req)))
-    (str "/cena/" (:minPrice req) "_" (:maxPrice req))))
-
-(defn construct-url
-  [req]
-  (str "https://www.nekretnine.rs/stambeni-objekti/stanovi/izdavanje-prodaja/izdavanje"
-       (construct-city-part req) (construct-city req) (construct-price req) "/lista/po-stranici/10/")
-  )
+  (def base-url (core/construct-base-url req))
+  (def page-count (-> base-url
+                      core/html-data
+                      core/get-result-count
+                      core/get-page-count))
+  (def url-list (core/construct-url-list base-url page-count))
+  (core/get-results url-list))
 
 (defroutes my_routes
-           (GET "/" [] (response "Pocetna"))
-           (GET "/hello" [] {:status  200
-                             :headers {"Content-Type" "application/json"}
-                             :body    "Hello"})
-           (GET "/rest" [] (response [{:ime "Pera" :prezime "Peric"} {:ime "Mika" :prezime "Mikic"}]))
-           (POST "/vue" [] (fn [req] (construct-url (:body req))))
+           (POST "/search" [] (fn [req] (search (:body req))))
            (route/resources "/"))
 
 (def app (wrap-json-body (wrap-cors (wrap-json-response (allow-cross-origin my_routes)) :access-control-allow-methods #{:get :post :delete :options}
